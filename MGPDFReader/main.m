0ee9a9ece7af9828b51b471f7b18aba61b9274ce
mige <mige@migedeiMac.local>
2018-05-18 17:27:14 +0800

    Initial Commit

---
+++ MGPDFReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MGPDFReader
+//
+//  Created by mige on 2018/5/18.
+//  Copyright © 2018年 mige.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
